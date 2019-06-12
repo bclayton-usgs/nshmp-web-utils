ab82b564f3802e2ddcd10eaf51fa854e6d8266b0
bclayton-usgs <bclayton@usgs.gov>
2019-06-12 10:33:46 -0600

    match new updates of service

--- lib/haz-ws/hazard-results-service/response/hazard-curves.ts
+++ lib/haz-ws/hazard-results-service/response/hazard-curves.ts
@@ -1,8 +1,10 @@
 import { EnumParameterValues } from '../../enum-parameter-values';
 import { HazardData } from './hazard-data';
+import { HazardDataType } from './hazard-data-type';
 
 export interface HazardCurves {
   imt: EnumParameterValues;
+  dataType: HazardDataType;
   imls: number[];
   data: HazardData[];
 }
